2ec2c21b01c9ba6f7cb4c68f891234eebd7d1959
admin <3ugol@ukr.net>
2019-03-13 22:21:29 +0200

    1test

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Stack;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String highAndLow = "1 2 -3 4 5";
+        int h, l;
+        //int x=Integer.parseInt(highAndLow[0]);
+
+            //return (stack.empty() && boo);
+        //System.out.println(stack.empty() && boo);
+
+    }
+}
