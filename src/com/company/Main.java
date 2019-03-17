fd51ccf01b20567df13415203daea35a3121b224
admin <3ugol@ukr.net>
2019-03-17 19:10:00 +0200

    serch minus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,41 +1,42 @@
 package com.company;
 import java.util.Stack;
-
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String highAndLow = "2 -9 7 3 4 1 5";
-        int h=0, z=0;
-        int l=Character.digit(highAndLow.charAt(0), 10);;
-        //char z='-';
-
+        String highAndLow = "-2 32 0 5";
+        int h=0, l=0, z=0, fn=0, sn=0;
+        List<Integer> list = new ArrayList<Integer>();
+        char a;
         for (int c=0; c < highAndLow.length(); c++ ){
-            if (highAndLow.charAt(c) != 32 ) {
-                System.out.println(highAndLow.charAt(c));
-                if(h < Character.digit(highAndLow.charAt(c), 10)){
-                    h=Character.digit(highAndLow.charAt(c), 10);
-                    //if (highAndLow.charAt(c-1) == '-' ){
-                    //    h=Math.abs(h);
-                    //}
-                }
-                if (l > Character.digit(highAndLow.charAt(c), 10)){
-                    l=Character.digit(highAndLow.charAt(c), 10);
-
-
-                }
-                z=c;
-                int n = Character.digit(highAndLow.charAt(c),10);
-                if (highAndLow.charAt(z--) == 45 ){
-                   // System.out.println(l);
-                    System.out.println("!- " + (l=n-(n*2)));
-                }
-
+            if (highAndLow.charAt(c) == '-'){
+                //a = highAndLow.charAt(c+1);
+                //z = a;
+                // = Integer.parseInt(args[0]);
+                z= Character.digit(highAndLow.charAt(c+1),10);
+                z=z-z*2;
+                list.add(z);
+                System.out.println("z= " + z);
+                break c++;
             }
+            System.out.println(c);
+            if (highAndLow.charAt(c) != ' ' && highAndLow.charAt(c) != '-'){
+                fn= Character.digit(highAndLow.charAt(c),10);
+                c++;
+                /*if (highAndLow.charAt(c) != ' '){
+                    sn= Character.digit(highAndLow.charAt(c),10);
+                    fn=fn*10;
+                    c++;
+
+                }*/
+                System.out.println(fn + " fn ");
+                //fn=fn+sn;
+                list.add(fn);
+            }
+
         }
-
-
-            System.out.println("Max " + h + " Min " + l );
-
+            //System.out.println("Max " + h + " Min " + l );
+        System.out.println(list);
     }
 }
