7398c9b4fbf5f81b62fa6d9d1f555b46b5e178c4
admin <3ugol@ukr.net>
2019-03-17 22:20:47 +0200

    search bug

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,38 +5,35 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String highAndLow = "-2 32 0 5";
-        int h=0, l=0, z=0, fn=0, sn=0;
+        String highAndLow = "-1 -1";
+        int h=0, l=0, z=0, fn=0, sn=0, c;
         List<Integer> list = new ArrayList<Integer>();
         char a;
-        for (int c=0; c < highAndLow.length(); c++ ){
-            if (highAndLow.charAt(c) == '-'){
-                //a = highAndLow.charAt(c+1);
-                //z = a;
-                // = Integer.parseInt(args[0]);
-                z= Character.digit(highAndLow.charAt(c+1),10);
-                z=z-z*2;
-                list.add(z);
-                System.out.println("z= " + z);
-                c++;
-            }
-            System.out.println(c);
+        for (c=0; c < highAndLow.length(); c++ ){
             if (highAndLow.charAt(c) != ' ' && highAndLow.charAt(c) != '-'){
                 fn= Character.digit(highAndLow.charAt(c),10);
+                // = Integer.parseInt(args[0]);
+                z=c;
                 c++;
-                /*if (highAndLow.charAt(c) != ' '){
-                    sn= Character.digit(highAndLow.charAt(c),10);
-                    fn=fn*10;
-                    c++;
-
-                }*/
-                System.out.println(fn + " fn ");
-                //fn=fn+sn;
+                if (c<highAndLow.length()) {
+                    if (highAndLow.charAt(c) != ' ') {
+                        sn = Character.digit(highAndLow.charAt(c), 10);
+                        fn = fn * 10 + sn;
+                        }
+                 }
+                if (z>1){
+                if (highAndLow.charAt(z-1) == '-'){
+                    fn=fn-fn*2;
+                }}
                 list.add(fn);
             }
-
         }
-            //System.out.println("Max " + h + " Min " + l );
+        for (int t=0; t < list.size(); t++ ){
+            h=Math.max(h, list.get(t));
+            l=Math.min(l, list.get(t));
+        }
+            System.out.println( String.format("%d %d", h, l) );
         System.out.println(list);
+        //return String.format("%02d %02d", h, l);
     }
 }
