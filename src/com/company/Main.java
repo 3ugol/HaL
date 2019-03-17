c705f9cf9c2c72def2b77ceb0d1af62e7dff8a59
admin <3ugol@ukr.net>
2019-03-17 14:17:05 +0200

    serch minus

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,41 @@
 package com.company;
 import java.util.Stack;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        String highAndLow = "1 2 -3 4 5";
-        int h, l;
-        //int x=Integer.parseInt(highAndLow[0]);
+        String highAndLow = "2 -9 7 3 4 1 5";
+        int h=0, z=0;
+        int l=Character.digit(highAndLow.charAt(0), 10);;
+        //char z='-';
 
-            //return (stack.empty() && boo);
-        //System.out.println(stack.empty() && boo);
+        for (int c=0; c < highAndLow.length(); c++ ){
+            if (highAndLow.charAt(c) != 32 ) {
+                System.out.println(highAndLow.charAt(c));
+                if(h < Character.digit(highAndLow.charAt(c), 10)){
+                    h=Character.digit(highAndLow.charAt(c), 10);
+                    //if (highAndLow.charAt(c-1) == '-' ){
+                    //    h=Math.abs(h);
+                    //}
+                }
+                if (l > Character.digit(highAndLow.charAt(c), 10)){
+                    l=Character.digit(highAndLow.charAt(c), 10);
+
+
+                }
+                z=c;
+                int n = Character.digit(highAndLow.charAt(c),10);
+                if (highAndLow.charAt(z--) == 45 ){
+                   // System.out.println(l);
+                    System.out.println("!- " + (l=n-(n*2)));
+                }
+
+            }
+        }
+
+
+            System.out.println("Max " + h + " Min " + l );
 
     }
 }
